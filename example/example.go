899ef8a4ce13d30728f4ed5c42f668ac0e373f04
ctengiz <cagatay.tengiz@gmail.com>
2024-05-07 14:40:52 +0300

    package rename

--- example/example.go
+++ example/example.go
@@ -5,8 +5,8 @@ import (
 	"log"
 	"time"
 
-	"github.com/hooklift/gowsdl/example/gen"
-	"github.com/hooklift/gowsdl/soap"
+	"github.com/ctengiz/gowsdl/example/gen"
+	"github.com/ctengiz/gowsdl/soap"
 )
 
 func ExampleBasicUsage() {
